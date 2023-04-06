92b595a4e0111a6a0c25539b68748d602594ea0e
sauyce <bryle95@hotmail.com>
2023-04-06 00:40:16 -0500

    Push empty project with .gitignore and .gitattributes

---
+++ Source/Hera/HeraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HeraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HERA_API AHeraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
